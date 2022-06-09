9d557ce314ef319b6a85ae05da87417ba07f0dde
Your Name <you@example.com>
2022-06-09 15:19:57 +0300

    script.sh

--- doc.sh
+++ doc.sh
@@ -12,4 +12,6 @@ sudo apt update
 curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
 chmod +x docker-compose-linux-x86_64
 sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
-rm docker-compose-linux-x86_64.sha256 
+rm docker-compose-linux-x86_64.sha256
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
