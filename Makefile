245bb98402e981bc45ec8e1e88999001d0ab83b1
Your Name <you@example.com>
2022-06-11 16:49:55 +0300

    finish

--- Makefile
+++ Makefile
@@ -6,12 +6,14 @@ PATH_DB	  = $(PATH_DATE)/db
 all: up
 
 up : 
-	sudo userdel www-data && sudo useradd -u 82 www-data
 	mkdir -p $(PATH_DATE) && sudo chmod 777 $(PATH_DATE)
 	mkdir -p $(PATH_WP) &&  sudo chmod 777 $(PATH_WP)
 	mkdir -p $(PATH_DB) &&  sudo chmod 777 $(PATH_DB)
 	sudo docker-compose -f  srcs/docker-compose.yaml build  --no-cache
 	sudo docker-compose -f  srcs/docker-compose.yaml up -d 
+
+set: 
+	echo "127.0.0.1 csherill.42.fr" | sudo tee -a /etc/hosts
 
 down:
 	sudo docker-compose -f srcs/docker-compose.yaml down
--- doc.sh
+++ doc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common lsb-release wget make 
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common lsb-release  wget make
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
